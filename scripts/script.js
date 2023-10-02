c892c41f33d10d865f23d5503fea9b343ad41e36
dempsterwinston <dempstercorpuz@gmail.com>
2023-10-03 00:18:12 +0800

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function onPress1() {
+  document.getElementById("onPress1").innerHTML = "Napisit!";
+  document.getElementById("onPress1").style.fontSize = "30px";
+}
+
+function onPress2() {
+  document.getElementById("onPress2").innerHTML = "Next Pisit!";
+  document.getElementById("onPress2").style.backgroundColor = "red";
+}
+function onPress3() {
+  document.getElementById("onPress3").innerHTML = "Next Next Pisit!";
+  document.getElementById("onPress3").style.color = "red";
+}
